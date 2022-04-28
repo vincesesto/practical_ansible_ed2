a389d3ab18f56b85b218f1bd2ec150580fc6643a
vincesesto <vince_sesto@mail.com>
2022-04-28 15:18:05 +1200

    Update user_data.sh

--- chapter_5/splunk_server/roles/file/user_data.sh
+++ chapter_5/splunk_server/roles/file/user_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e -x
-wget -O splunk-8.0.5-a1a6394cc5ae-linux-2.6-x86_64.rpm 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.0.5&product=splunk&filename=splunk-8.0.5-a1a6394cc5ae-linux-2.6-x86_64.rpm&wget=true'
-rpm -i splunk-8.0.5-a1a6394cc5ae-linux-2.6-x86_64.rpm
+wget -O splunk-8.2.6-a6fe1ee8894b-linux-2.6-x86_64.rpm "https://download.splunk.com/products/splunk/releases/8.2.6/linux/splunk-8.2.6-a6fe1ee8894b-linux-2.6-x86_64.rpm"
+rpm -i splunk-8.2.6-a6fe1ee8894b-linux-2.6-x86_64.rpm
 sleep 30
 sudo -u splunk /opt/splunk/bin/splunk start  --answer-yes --no-prompt --accept-license  --seed-passwd newpassword
